632826c337e10f8793681a074d4cf45e406b7dba
Daniil Tkachev <daniil.tkachev@oxid-esales.com>
2025-11-22 00:22:50 +0100

    new order

---
+++ docs/devday251121/_generated_e2e_example/paypal-payment-test.spec.ts
@@ -0,0 +1,69 @@
+import { test, expect } from '@playwright/test';
+
+test('paypal-payment-test', async ({ page }) => {
+  // Generated from validated OXTest
+
+  // navigate
+  await page.goto('https://osc2.oxid.shop');
+
+  // click
+  await page.getByText('Anmelden').click();
+
+  // type
+  await page.getByPlaceholder('E-Mail-Adresse').fill('redrobot@dantweb.dev');
+
+  // type
+  await page.getByPlaceholder('Passwort').fill('useruser');
+
+  // click
+  await page.getByText('Anmelden').click();
+
+  // navigate
+  await page.goto('https://osc2.oxid.shop/shop/');
+
+  // navigate
+  await page.goto('https://osc2.oxid.shop/index.php?cl=order');
+
+  // navigate
+  await page.goto('https://osc2.oxid.shop/index.php?cl=payment');
+
+  // click
+  await page.getByText('Click').click();
+
+  // waitForSelector
+  await page.locator('xpath=//iframe[@title=PayPal').waitFor({ state: 'visible' });
+
+  // waitForSelector
+  await page.locator('iframe[title=PayPal]').waitFor({ state: 'visible' });
+
+  // waitForSelector
+  await page.locator('iframe[title=PayPal]').waitFor({ state: 'visible' });
+
+  // waitForSelector
+  await page.locator('iframe[src*=paypal]').waitFor({ state: 'visible' });
+
+  // waitForSelector
+  await page.locator('iframe[src*=paypal]').waitFor({ state: 'visible' });
+
+  // waitForSelector
+  await page.locator('iframe[title=PayPal],').waitFor({ state: 'visible' });
+
+  // waitForSelector
+  await page.locator('xpath=//iframe[@title=PayPal').waitFor({ state: 'visible' });
+
+  // waitForSelector
+  await page.locator('xpath=//iframe[@title=PayPal').waitFor({ state: 'visible' });
+
+  // waitForSelector
+  await page.locator('iframe[title=PayPal]').waitFor({ state: 'visible' });
+
+  // waitForSelector
+  await page.locator('iframe[title=PayPal]').waitFor({ state: 'visible' });
+
+  // navigate
+  await page.goto('https://osc2.oxid.shop/checkout');
+
+  // wait
+  await page.waitForTimeout(30000);
+
+});
